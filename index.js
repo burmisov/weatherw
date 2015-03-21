ed8d8181046909642a1dc6b5e1165d4c29862232
burmisov <burmisov@informatica.ru>
2015-03-21 12:03:43 +0300

    изображения для виджета

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var debug = require('debug')('weatherw:index');
 var express = require('express');
+
 var webRouter = require('./lib/web/router');
+var widgetRouter = require('./lib/widget/router');
 
 debug('setting up http server');
 var app = express();
-app.use(webRouter);
+
+app.use('/', webRouter);
+app.use('/widget', widgetRouter);
+
 var port = process.env.PORT || 4000;
 app.listen(port);
 console.log('Weather-widget server lisetning on port', port);
